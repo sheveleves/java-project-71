0c2e9f57e4d703263dd48fe0e776e9730e46c0b2
sheveleves <shevelve@gmail.com>
2023-01-22 17:07:32 +0500

    add generateReport

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -14,8 +14,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Differ {
+    private static Map<String, Object> mapFirstFile;
+    private static Map<String, Object> mapSecondFile;
+    private List<String> sortedKeys;
 
-    public static Map<String, Object> readFileToMap(final String filepath) throws IOException {
+    private static Map<String, Object> readFileToMap(final String filepath) throws IOException {
         Path path = Paths.get(filepath);
         String str = Files.readString(path);
         if (str.equals("")) {
@@ -25,18 +28,9 @@ public class Differ {
         return objectMapper.readValue(str, new TypeReference<Map<String, Object>>() { });
     }
 
-    public static String generate(String filepath1, String filepath2) throws IOException {
-        Map<String, Object> mapFirstFile = readFileToMap(filepath1);
-        Map<String, Object> mapSecondFile = readFileToMap(filepath2);
-        List<String> sortedKeys = new ArrayList<>(mapFirstFile.keySet());
-        for (String key: mapSecondFile.keySet()) {
-            if (!sortedKeys.contains(key)) {
-                sortedKeys.add(key);
-            }
-        }
-        Collections.sort(sortedKeys);
+    private static String generateReport(List<String> keys) {
         var result = new StringBuilder("{\n");
-        for (String key: sortedKeys) {
+        for (String key: keys) {
             if (!mapFirstFile.containsKey(key)) {
                 result.append("  + " + key + ": " + mapSecondFile.get(key).toString() + "\n");
             } else if (!mapSecondFile.containsKey(key)) {
@@ -48,8 +42,21 @@ public class Differ {
                         .append("  + " + key + ": " + mapSecondFile.get(key).toString() + "\n");
             }
         }
-
         result.append("}");
         return result.toString();
+    }
+
+
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        mapFirstFile = readFileToMap(filepath1);
+        mapSecondFile = readFileToMap(filepath2);
+        List<String> sortedKeys = new ArrayList<>(mapFirstFile.keySet());
+        for (String key: mapSecondFile.keySet()) {
+            if (!sortedKeys.contains(key)) {
+                sortedKeys.add(key);
+            }
+        }
+        Collections.sort(sortedKeys);
+        return generateReport(sortedKeys);
     }
 }
