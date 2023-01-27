3d20cd48346fe7a49d8b931ff5347e3c9aa81f25
sheveleves <shevelve@gmail.com>
2023-01-27 09:52:58 +0500

    add Parser and resourse file

---
+++ app/src/main/java/hexlet/code/Parser.java
@@ -0,0 +1,31 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Parser {
+    private static Map<String, Object> readFileToMap(final String filepath) throws IOException {
+        Path path = Paths.get(filepath).toAbsolutePath().normalize();
+        String str = Files.readString(path);
+        if (str.equals("")) {
+            return new HashMap<String, Object>();
+        }
+        ObjectMapper objectMapper = new YAMLMapper();
+        return objectMapper.readValue(str, new TypeReference<Map<String, Object>>() { });
+    }
+
+    public static String generate(String filepath1, String filepath2) throws IOException {
+        App.setMapFirstFile(readFileToMap(filepath1));
+        App.setMapSecondFile(readFileToMap(filepath2));
+        App.takeSortedKey();
+        return App.generateReport(App.getSortedKeys());
+    }
+}
